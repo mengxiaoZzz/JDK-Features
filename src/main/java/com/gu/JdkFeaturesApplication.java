6b8e30cd9c8143ff287adf0db1ba7106a2efb2a5
guyuqiang <1243952362@qq.com>
2020-03-30 23:30:03 +0800

    项目搭建

---
+++ src/main/java/com/gu/JdkFeaturesApplication.java
@@ -0,0 +1,13 @@
+package com.gu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdkFeaturesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdkFeaturesApplication.class, args);
+    }
+
+}
